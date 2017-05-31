e924ec1f9cc64e012cbb468479136e191490a89e
aasthagrover <grover.aa@husky.neu.edu>
2017-05-30 21:41:00 -0400

    Submission Version 2

--- Question1/Question1.py
+++ Question1/Question1.py
@@ -1,58 +1,74 @@
 # Question 1
 
-#Read data from the csv File
-import os
-x = input()
-file=os.path.normpath(os.path.join(x, 'data.csv'))
-import pandas as pd
-read_data = pd.read_csv(file)
+#Function to read data from the csv file
+def readFile():
+    import os
+    try:
+        x = input()
+        file=os.path.normpath(os.path.join(x, 'data.csv'))
+        import pandas as pd
+        read_data = pd.read_csv(file)
+        return read_data
+        pass
+    except OSError:
+        print("Path not valid. Please enter the path again")
+        return readFile()
+    
+    
+def main():
+    read_data = readFile()
+    import pandas as pd
+    #1.Filter Gender code for Males & Females
+    #2. Group by State codes to count the number of Males & Females in each State.
+    females=pd.DataFrame(read_data.loc[read_data['Gender Code from Claim'] == 1].groupby('State Code from Claim (SSA)')['Gender Code from Claim'].count())
+    females['State']=females.index
+    females.columns=['Females','State']
 
-#1.Filter Gender code for Males & Females
-#2. Group by State codes to count the number of Males & Females in each State.
-females=pd.DataFrame(read_data.loc[read_data['Gender Code from Claim'] == 1].groupby('State Code from Claim (SSA)')['Gender Code from Claim'].count())
-females['State']=females.index
-females.columns=['Females','State']
-
-males=pd.DataFrame(read_data.loc[read_data['Gender Code from Claim'] == 2].groupby('State Code from Claim (SSA)')['Gender Code from Claim'].count())
-males['State']=males.index
-males.columns=['Males','State']
-
-
-#1.Filter the column 'LDS Age Category' and make categories for Ages (<65,65-74,75+). 
-#'LDS Age Category' for Age<65 = 1
-#'LDS Age Category' for 65-74 = 2 & 3 
-#'LDS Age Category' for 75+ = 4 & 5 & 6
-#2. Group by State codes to count the number of people in each age category.
-
-age_lessthan_65=pd.DataFrame(read_data.loc[read_data['LDS Age Category']==1].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
-age_lessthan_65['State']=age_lessthan_65.index
-age_lessthan_65.columns=['Ages < 65','State']
-
-age_between_65to74=pd.DataFrame(read_data.loc[read_data['LDS Age Category'].isin([2,3])].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
-age_between_65to74['State']=age_between_65to74.index
-age_between_65to74.columns=['Ages 65-74','State']
-
-age_greaterthan_75=pd.DataFrame(read_data.loc[read_data['LDS Age Category'].isin([4,5,6])].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
-age_greaterthan_75['State']=age_greaterthan_75.index
-age_greaterthan_75.columns=['Ages 75+','State']
+    males=pd.DataFrame(read_data.loc[read_data['Gender Code from Claim'] == 2].groupby('State Code from Claim (SSA)')['Gender Code from Claim'].count())
+    males['State']=males.index
+    males.columns=['Males','State']
 
 
-#Merge all the dataframes on column 'State' 
+    #1.Filter the column 'LDS Age Category' and make categories for Ages (<65,65-74,75+). 
+    #'LDS Age Category' for Age<65 = 1
+    #'LDS Age Category' for 65-74 = 2 & 3 
+    #'LDS Age Category' for 75+ = 4 & 5 & 6
+    #2. Group by State codes to count the number of people in each age category.
 
-final_output=pd.merge(pd.merge(pd.merge(pd.merge(females, males, on='State', how='outer'),age_lessthan_65,on='State', how='outer'),age_between_65to74,on='State', how='outer'),age_greaterthan_75,on='State', how='outer').fillna(0)
+    age_lessthan_65=pd.DataFrame(read_data.loc[read_data['LDS Age Category']==1].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
+    age_lessthan_65['State']=age_lessthan_65.index
+    age_lessthan_65.columns=['Ages < 65','State']
 
-#Rearrange the order of columns for writing to csv file
-column_list=final_output.columns.tolist()
+    age_between_65to74=pd.DataFrame(read_data.loc[read_data['LDS Age Category'].isin([2,3])].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
+    age_between_65to74['State']=age_between_65to74.index
+    age_between_65to74.columns=['Ages 65-74','State']
 
-cols=[column_list[1],column_list[0],column_list[2],column_list[3],column_list[4],column_list[5]]
+    age_greaterthan_75=pd.DataFrame(read_data.loc[read_data['LDS Age Category'].isin([4,5,6])].groupby('State Code from Claim (SSA)')['LDS Age Category'].count())
+    age_greaterthan_75['State']=age_greaterthan_75.index
+    age_greaterthan_75.columns=['Ages 75+','State']
 
-final_output[cols].to_csv('output1.csv',index = False)
 
+    #Merge all the dataframes on column 'State' 
 
-# If the output file with same name is already present replace it
-try:
-    final_output[cols].to_csv('output1.csv',index = False)   
-except OSError:
-    pass
-    os.remove(filename)
+    final_output=pd.merge(pd.merge(pd.merge(pd.merge(females, males, on='State', how='outer'),age_lessthan_65,on='State', how='outer'),age_between_65to74,on='State', how='outer'),age_greaterthan_75,on='State', how='outer').fillna(0)
+
+    #Rearrange the order of columns for writing to csv file
+    column_list=final_output.columns.tolist()
+
+    cols=[column_list[1],column_list[0],column_list[2],column_list[3],column_list[4],column_list[5]]
+
     final_output[cols].to_csv('output1.csv',index = False)
+
+
+    # Handle the exception if file with the same name as 'output2' is already present in the directory. Replace the file with new file
+    try:
+        final_output[cols].to_csv('output1.csv',index = False)   
+    except OSError:
+        pass
+        os.remove(filename)
+        final_output[cols].to_csv('output1.csv',index = False)
+
+    
+#Execute the main function    
+if __name__ == "__main__": main()
+    
--- Question2/Question2.py
+++ Question2/Question2.py
@@ -1,73 +1,93 @@
 # Question 2-Extra Credit Question
 
-#Read data from the csv File
-import os
-x = input()
-file=os.path.normpath(os.path.join(x, 'data.csv'))
-import pandas as pd
-read_data = pd.read_csv(file)
-
-# Group the data by Claim utilization Day count &
-#Count the number of claims for each category where  categories are made when Claim utilization days are same.
-
-total_claims_for_eachnumberof_utilization_day = pd.DataFrame(read_data.groupby('Claim Utilization Day Count')['Claim number'].count())
-total_claims_for_eachnumberof_utilization_day['Utilization Range'] = total_claims_for_eachnumberof_utilization_day.index
-total_claims_for_eachnumberof_utilization_day.columns = ['Counts','Utilization Range']
-
-# Make a list of all the Claim Utilization days.
-all_utilization_day_ranges = []
-
-for r in range(1,max(total_claims_for_eachnumberof_utilization_day['Utilization Range'])+1):
-    all_utilization_day_ranges.append(r)
-    
-# Calculate the sum of all the claims for any number of the Claim Utilization days  
-total_sum_of_all_claims = total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range'].isin(all_utilization_day_ranges),'Counts'].sum()
-
-
-# Make a list 'sum_of_claims_over_each_utilization_range' which contains the sum of the number of claims for each Claim Utilization day range.
-sum_of_claims_over_each_utilization_range = []
-
-# The categories for Claim Utilization day range are as follows: 0,1,2,3,4,5,6-10,11,30, greater than equal to 30 days
-#all_ranges=[[0],[1],[2],[3],[4],[5],list(range(6,11)),list(range(11,31)),list(range(30,max(total_claims_for_eachnumberof_utilization_day['Utilization Range'])+1))]
-
-range6_10=[]
-range11_30=[]
-range30_plus=[]
-
-for value in range(0,6):
-    sum_of_claims_over_each_utilization_range.append(total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range']==value,'Counts'].sum())
-
-for i in range(6,11):
-    range6_10.append(i)
-    
-for j in range(11,31):
-    range11_30.append(j)
-    
-for k in range(30,max(total_claims_for_eachnumberof_utilization_day['Utilization Range'])+1):
-    range30_plus.append(k)
-    
-all_ranges=[range6_10,range11_30,range30_plus]
-
-for item in all_ranges:
-    sum_of_claims_over_each_utilization_range.append(total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range'].isin(item),'Counts'].sum())
+#Function to read data from the csv file
+def readFile():
+    import os
+    try:
+        x = input()
+        file=os.path.normpath(os.path.join(x, 'data.csv'))
+        import pandas as pd
+        read_data = pd.read_csv(file)
+        return read_data
+        pass
+    except OSError:
+        print("Path not valid. Please enter the path again")
+        return readFile() # if error in reading file, ask for user input for filepath again .
     
     
-# Make a list 'count_list' which contains the percentages of claims in each category range of Claim Utilization Day.
-percentage_list=[]
+def main():
+    try:
+        read_data = readFile()
+        import pandas as pd
+        # 1. Group the data by Claim utilization Day count
+        # 2. Count the number of claims for each category of claim utilization day where  categories are made when Claim utilization days are same.
+        # 3. Name the dataframe as 'total_claims_for_eachnumberof_utilization_day' which contains count of claims for each category of claim utilization day
+        total_claims_for_eachnumberof_utilization_day = pd.DataFrame(read_data.groupby('Claim Utilization Day Count')['Claim number'].count())
+        total_claims_for_eachnumberof_utilization_day['Utilization Range'] = total_claims_for_eachnumberof_utilization_day.index
+        total_claims_for_eachnumberof_utilization_day.columns = ['Counts','Utilization Range']
 
-for i,count in enumerate(sum_of_claims_over_each_utilization_range):
-    percentage_list.append((count*100)/total_sum_of_all_claims)
-    
+        # Make a list of all the unique Claim Utilization days.
+        all_utilization_day_ranges = []
+        for r in range(1,max(total_claims_for_eachnumberof_utilization_day['Utilization Range'])+1):
+            all_utilization_day_ranges.append(r)
 
-# Make a dataframe which contains teh Claim Utilization Day Range, Count of claims for each Utilization Day Range, 
-# Percentage of Claims for each range
-final_frame=pd.DataFrame({'Utilization Range':["0","1","2","3","4","5","'6-10'","'11-30'","30 Plus"],'Counts':sum_of_claims_over_each_utilization_range,'Percentages':percentage_list})
-col=final_frame.columns.tolist()
-final_columns=[col[2],col[0],col[1]]
+        # Calculate the sum of all the claims for any number of the Claim Utilization days  
+        total_sum_of_all_claims = total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range'].isin(all_utilization_day_ranges),'Counts'].sum()
 
-try:
-    final_frame[final_columns].to_csv('output2.csv',index = False)   
-except OSError:
-    pass
-    os.remove(filename)
-    final_frame[final_columns].to_csv('output2.csv',index = False)
+
+        # Make a list 'sum_of_claims_over_each_utilization_range' which contains the sum of the number of claims for each individual Claim Utilization day range.
+        sum_of_claims_over_each_utilization_range = []
+
+        # The categories for Claim Utilization day range are as follows: 0,1,2,3,4,5,6-10,11,30, greater than equal to 30 days
+        # for categories where claim utilization days are to be taken as(Example : [6-10], [11-30],[30 and above]). Make 3 lists to contain the range numbers 
+        range6_10=[]
+        range11_30=[]
+        range30_plus=[]
+
+        #append the count of number of claims for 0-5 claim utilization days in a list 'sum_of_claims_over_each_utilization_range'
+        for value in range(0,6):
+            sum_of_claims_over_each_utilization_range.append(total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range']==value,'Counts'].sum())
+
+        for i in range(6,11):
+            range6_10.append(i)
+
+        for j in range(11,31):
+            range11_30.append(j)
+
+        for k in range(30,max(total_claims_for_eachnumberof_utilization_day['Utilization Range'])+1):
+            range30_plus.append(k)
+
+        #all_ranges list contains the lists for ranges of claim utilization days. Example: [6-10], [11-30],[30 and above]
+        all_ranges=[range6_10,range11_30,range30_plus]
+
+        #append the count of number of claims for [6-10],[11-30],[30 and above] claim utilization day ranges a list 'sum_of_claims_over_each_utilization_range'
+        for item in all_ranges:
+            sum_of_claims_over_each_utilization_range.append(total_claims_for_eachnumberof_utilization_day.loc[total_claims_for_eachnumberof_utilization_day['Utilization Range'].isin(item),'Counts'].sum())
+
+
+        # Make a list 'percentage_list' which contains the percentages of claims in each category range of Claim Utilization Day.
+        percentage_list=[]
+
+        for i,count in enumerate(sum_of_claims_over_each_utilization_range):
+            percentage_list.append((count*100)/total_sum_of_all_claims)
+
+
+        # Make a dataframe which contains teh Claim Utilization Day Range, Count of claims for each Utilization Day Range, 
+        # Percentage of Claims for each range
+        final_frame=pd.DataFrame({'Utilization Range':["0","1","2","3","4","5","'6-10'","'11-30'","30 Plus"],'Counts':sum_of_claims_over_each_utilization_range,'Percentages':percentage_list})
+        col=final_frame.columns.tolist()
+        final_columns=[col[2],col[0],col[1]]
+        
+        # Handle the exception if file with the same name as 'output2' is already present in the directory. Replace the file with new file
+        try:
+            final_frame[final_columns].to_csv('output2.csv',index = False)   
+        except OSError:
+            pass
+            os.remove(filename)
+            final_frame[final_columns].to_csv('output2.csv',index = False)
+    except:
+        print("Unexpected Error")
+        
+        
+#Execute the main function         
+if __name__ == "__main__": main()
